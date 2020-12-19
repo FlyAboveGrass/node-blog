c2829c28ef37e0de6aa64afc10f64eb99f9f569f
yangjiajian <287023075@qq.com>
2020-12-19 23:07:43 +0800

    添加部分路由以及书写笔记

---
+++ node-blog/readme.md
@@ -0,0 +1,7 @@
+## 项目的使用
+```
+    npm install
+    npm run dev
+```
+## 项目的依赖
+mysql数据库
--- node-blog/src/router/blog.js
+++ node-blog/src/router/blog.js
@@ -1,10 +1,27 @@
 const { getBlogList } = require('../controller/blog')
-const { SuccessModel } = require('../model/resModel')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleBlogRouter = (req, res) => {
+    console.log('file: blog.js ~ line 5 ~ handleBlogRouter ~ req', req);
     if(req.url === '/blog' && req.method === 'GET'){
         const data =  getBlogList(req)
         return new SuccessModel(data, '获取博客列表成功')
+    }
+    if(req.url === '/api/blog/detail' && req.method === 'POST'){
+        const data =  getBlogList(req)
+        return data ? new SuccessModel(data, '获取博客成功') : new ErrorModel(data, '获取博客失败')
+    }
+    if(req.url === '/api/blog/new' && req.method === 'POST'){
+        const data =  getBlogList(req)
+        return data ? new SuccessModel(data, '新建博客成功') : new ErrorModel(data, '新建博客失败')
+    }
+    if(req.url === '/api/blog/update' && req.method === 'POST'){
+        const data =  getBlogList(req)
+        return data ? new SuccessModel(data, '更新博客列表成功') : new ErrorModel(data, '更新博客列表失败')
+    }
+    if(req.url === '/api/blog/del' && req.method === 'POST'){
+        const data =  getBlogList(req)
+        return data ? new SuccessModel(data, '删除博客列表成功') : new ErrorModel(data, '删除博客列表失败')
     }
 }
 
